4a51a2664cee623b2a54f5164ef30883e7c18606
Ubuntu <ubuntu@conda1.novalocal>
2026-01-14 20:59:48 +0000

    Add PSMPI version vendor-4.3.2 with bundled shs-libfabric for Slingshot-11

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,6 @@ export CC=$(basename "$CC")
 export CXX=$(basename "$CXX")
 export FC=$(basename "$FC")
 
-cd $SRC_DIR/psmpi
-
 if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
   export CROSS_F77_SIZEOF_INTEGER=4
   export CROSS_F77_SIZEOF_REAL=4
@@ -21,22 +19,59 @@ if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
   export CROSS_F90_DOUBLE_MODEL=" 15 , 307"
 fi
 
+# Build shs-libfabric (non-CUDA version)
+cd ${SRC_DIR}/shs-libfabric
+
+autoreconf -ivf
+
+./configure --prefix=${PREFIX} \
+            --enable-cxi \
+            --with-cassini-headers=${PREFIX} \
+            --with-cxi-uapi-headers=${PREFIX} \
+            --with-curl=${PREFIX} \
+            --with-json-c=${PREFIX} \
+            --with-libnl=${PREFIX} \
+            --docdir=$PWD/noinst/doc \
+            --mandir=$PWD/noinst/man \
+            --disable-lpp \
+            --disable-psm3 \
+            --disable-opx \
+            --disable-efa \
+            --disable-static
+
+make -j${CPU_COUNT} src/libfabric.la
+make -j${CPU_COUNT} util/fi_info util/fi_pingpong util/fi_strerror util/fi_mon_sampler
+
+make install-exec install-data
+
+# Build psmpi (vendor branch)
+cd ${SRC_DIR}/psmpi/mpich2
+
 ./autogen.sh
 
 mkdir -p build
 cd build
 
-../configure --prefix=$PREFIX \
-             --build=${BUILD} \
-             --host=${HOST} \
-             --with-confset=gcc \
-             --enable-confset-overwrite \
-             --with-pscom-allin=$SRC_DIR/pscom \
-             --with-hwloc=$PREFIX \
-             --with-pmix=$PREFIX \
-             --enable-msa-awareness \
-             --enable-threading
+../configure --prefix="${PREFIX}" \
+             --build="${BUILD}" \
+             --host="${HOST}" \
+             --with-device=ch4:ucx,ofi \
+             --with-ucx="${PREFIX}" \
+             --with-libfabric="${PREFIX}" \
+             --with-hwloc="${PREFIX}" \
+             --with-pmix="${PREFIX}" \
+             --with-pmi=pmix \
+             --with-pm=none \
+             --enable-threads=runtime \
+             --enable-romio \
+             --enable-cxx \
+             --enable-fast=all \
+             --enable-g=none \
+	     --disable-doc \
+	     --disable-static \
+	     --disable-dependency-tracking \
+	     --with-wrapper-dl-type=none
 
-make -j"${CPU_COUNT}"
+make -j${CPU_COUNT}
 
 make install
